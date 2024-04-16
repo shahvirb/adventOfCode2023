12a0407cbf5e2cf0e97644090af390d3206f62f1
Shahvir Buhariwalla <shahvirb@gmail.com>
2024-04-16 18:42:48 -0500

    Some test refactoring and adding a wrong answer for day 1 part 2 for the sake of TDD

--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,13 +1,28 @@
 from adventofcode2023 import day1, aocinput
 
 
-def check_answer(stem: str, solver):
-    input = aocinput.read_filename_lines(stem + ".input")
-    assert solver(input) == int(aocinput.read_answer(stem))
+def check_answer(input_name: str, solver, answer_name: str = None):
+    input = aocinput.read_filename_lines(input_name + ".input")
+    if not answer_name:
+        answer_name = input_name
+    assert solver(input) == int(aocinput.read_answer(answer_name))
 
 
 def test_day1():
-    TESTS = [("day1", day1.solve_p1), ("day1.part2sample", day1.solve_p2)]
-    for filestem, solver_func in TESTS:
-        check_answer(filestem, solver_func)
+    from dataclasses import dataclass
+    from typing import Callable
+
+    @dataclass
+    class TestCase:
+        input: str
+        solver_func: Callable
+        answer: str = None
+
+    TESTS = [
+        TestCase("day1", day1.solve_p1),
+        TestCase("day1.part2sample", day1.solve_p2),
+        TestCase("day1", day1.solve_p2, "day1.part2"),
+    ]
+    for tc in TESTS:
+        check_answer(tc.input, tc.solver_func, tc.answer)
     # TODO this is not a pytest-onic way defining and running tests.
