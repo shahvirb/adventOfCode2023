044cb73cbd1d7b8aabd4f768d39ce5134f253f82
Shahvir Buhariwalla <shahvirb@gmail.com>
2024-04-20 18:07:07 -0500

    Using parametrize with seperate parameters so that it shows up more intuitively in vscode

--- tests/test_day.py
+++ tests/test_day.py
@@ -1,38 +1,29 @@
 from adventofcode2023 import aocinput
 from adventofcode2023 import day1, day2, day3
-from dataclasses import dataclass
-from typing import Callable
 import pytest
 
 # TODO how can we use pytest-mypy to run mypy during tests?
 
 
-@dataclass
-class TestCase:
-    input: str
-    solver_func: Callable
-    answer: str = None
-
-
 @pytest.mark.parametrize(
-    "tc",
+    "input, solver_func, answer",
     [
         # Day 1
-        TestCase("day1", day1.solve_p1),
-        TestCase("day1.part2sample", day1.solve_p2),
-        TestCase("day1", day1.solve_p2, "day1.part2"),
+        ("day1", day1.solve_p1, None),
+        ("day1.part2sample", day1.solve_p2, None),
+        ("day1", day1.solve_p2, "day1.part2"),
         # Day 2
-        TestCase("day2.part1sample", day2.solve_p1),
-        TestCase("day2", day2.solve_p1),
-        TestCase("day2.part1sample", day2.solve_p2, "day2.part2sample"),
-        TestCase("day2", day2.solve_p2, "day2.part2"),
+        ("day2.part1sample", day2.solve_p1, None),
+        ("day2", day2.solve_p1, None),
+        ("day2.part1sample", day2.solve_p2, "day2.part2sample"),
+        ("day2", day2.solve_p2, "day2.part2"),
         # Day 3
-        TestCase("day3.part1sample", day3.solve_p1),
-        TestCase("day3", day3.solve_p1),
+        ("day3.part1sample", day3.solve_p1, None),
+        ("day3", day3.solve_p1, None),
     ],
 )
-def test_solve(tc):
-    input_data = aocinput.read_filename_lines(tc.input + ".input")
-    assert tc.solver_func(input_data) == int(
-        aocinput.read_answer(tc.answer if tc.answer else tc.input)
+def test_solve(input, solver_func, answer):
+    input_data = aocinput.read_filename_lines(input + ".input")
+    assert solver_func(input_data) == int(
+        aocinput.read_answer(answer if answer else input)
     )
