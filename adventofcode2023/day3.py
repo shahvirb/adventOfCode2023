63ea7a0cdb925f5961602a278a68c5e1310daa4c
Shahvir Buhariwalla <shahvirb@gmail.com>
2024-04-20 17:53:43 -0500

    Solving day 3 part 1

--- adventofcode2023/day3.py
+++ adventofcode2023/day3.py
@@ -1,4 +1,8 @@
 import typing
+import re
+
+MAX_LENGTH_PARTNUMBER = 3
+NUMBERS_REGEX = re.compile(r"\d+")
 
 
 def is_symbol(char: str):
@@ -28,13 +32,28 @@ def plus_minus(x: int, limit_min: int, limit_max: int) -> tuple[int, int]:
     return low, high
 
 
+def within_match(match: re.Match, i: int) -> bool:
+    """Checks whether i's value is within/inside the span of the match
+
+    Args:
+        match (re.Match): The regex match to test
+        i (int): some integer value
+
+    Returns:
+        bool: True if i is within the span of the match
+    """
+    return match.start() <= i < match.end()
+
+
 # When accessing do something like Grid.array[r][c]
 # where r is the row index and c is the col index
 class Schematic:
     def __init__(self, lines: list[str]) -> None:
-        self.array = [[c for c in line.strip()] for line in lines]
-        self.rows = len(self.array)
-        self.cols = len(self.array[0])
+        self.__array = [[c for c in line.strip()] for line in lines]
+        self.__lines = lines
+        # TODO do we really need both? array and lines?
+        self.rows = len(self.__array)
+        self.cols = len(self.__array[0])
         # Warning: don't change the array because rows and cols must then be recomputed
 
     # @property
@@ -48,24 +67,48 @@ class Schematic:
     def find_symbols(self) -> typing.Iterator[tuple[int, int]]:
         for r in range(self.rows):
             for c in range(self.cols):
-                if is_symbol(self.array[r][c]):
+                if is_symbol(self.__array[r][c]):
                     yield r, c
 
-    def adjacent_numbers(self, row, col) -> typing.Iterator[tuple[int, int]]:
+    def adjacent_digits(self, row, col) -> typing.Iterator[tuple[int, int]]:
         r1, r2 = plus_minus(row, 0, self.rows)
         c1, c2 = plus_minus(col, 0, self.cols)
         for i in range(r1, r2 + 1):
             for j in range(c1, c2 + 1):
-                if self.array[i][j].isnumeric():
+                if self.__array[i][j].isnumeric():
                     yield i, j
 
-    def calc_sum(self) -> int:
+    def find_digits(self) -> list[tuple[int, int]]:
+        digits_found = []
         for r, c in self.find_symbols():
-            print("Searching for symbols around: ", r, c)
-            for ar, ac in self.adjacent_numbers(r, c):
-                print("Found: ", self.array[ar][ac], ar, ac)
-                    
-        return 0
+            # print("Searching for symbols around: ", r, c)
+            for ar, ac in self.adjacent_digits(r, c):
+                # print("Found: ", self.__array[ar][ac], ar, ac)
+                digits_found.append((ar, ac))
+        return digits_found
+
+    def calc_sum(self) -> int:
+        adjacent_pns = set()
+        partnumbers_by_row = (
+            {}
+        )  # this will have keys as row numbers and values which are the regex matches of part numbers in that row
+        for r, c in self.find_digits():
+            if r not in partnumbers_by_row:
+                partnumbers_by_row[r] = [
+                    m for m in NUMBERS_REGEX.finditer(self.__lines[r])
+                ]
+            for match in partnumbers_by_row[r]:
+                if within_match(match, c):
+                    adjacent_pns.add(match)
+
+        sum = 0
+        for pn in adjacent_pns:
+            sum += int(pn[0])
+        return sum
+
+
+def solve_p1(lines: list[str]) -> int:
+    return Schematic(lines).calc_sum()
 
 
 if __name__ == "__main__":
@@ -77,4 +120,8 @@ if __name__ == "__main__":
 
     lines = aocinput.read_filename_lines("day3.part1sample.input")
     sch = Schematic(lines)
-    sch.calc_sum()
+    print(sch.calc_sum())
+
+    # for match in regex.finditer(lines[0]):
+    #     print(match.span())
+    # pass
--- tests/test_day.py
+++ tests/test_day.py
@@ -1,10 +1,11 @@
 from adventofcode2023 import aocinput
-from adventofcode2023 import day1, day2
+from adventofcode2023 import day1, day2, day3
 from dataclasses import dataclass
 from typing import Callable
 import pytest
 
-#TODO how can we use pytest-mypy to run mypy during tests?
+# TODO how can we use pytest-mypy to run mypy during tests?
+
 
 @dataclass
 class TestCase:
@@ -25,6 +26,9 @@ class TestCase:
         TestCase("day2", day2.solve_p1),
         TestCase("day2.part1sample", day2.solve_p2, "day2.part2sample"),
         TestCase("day2", day2.solve_p2, "day2.part2"),
+        # Day 3
+        TestCase("day3.part1sample", day3.solve_p1),
+        TestCase("day3", day3.solve_p1),
     ],
 )
 def test_solve(tc):
